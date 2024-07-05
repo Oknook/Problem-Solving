c5cb1f9552c69f2d7658ae1c6c1266f62317659c
Oknook <wjsguscjf23@naver.com>
2024-07-05 15:10:53 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 156 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -1,44 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int N = scanner.nextInt(); // 수열의 크기
-        int[] A = new int[N];
-        
-        for (int i = 0; i < N; i++) {
-            A[i] = scanner.nextInt(); // 수열의 원소들
-        }
-        
-        System.out.println(maxSumIncreasingSubsequence(A, N));
-    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    public static int maxSumIncreasingSubsequence(int[] A, int N) {
-        int[] dp = new int[N];
-        
-        // 초기화: dp[i]는 A[i] 자체로 끝나는 증가 부분 수열의 합
-        for (int i = 0; i < N; i++) {
-            dp[i] = A[i];
+        int a[][] = new int[2][n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[0][i] = Integer.parseInt(st.nextToken());
+            a[1][i] = a[0][i];
         }
 
-        // 동적 프로그래밍으로 dp 배열 채우기
-        for (int i = 1; i < N; i++) {
+        int answer = a[1][0];
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (A[i] > A[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + A[i]);
-                }
+                if (a[0][i] > a[0][j]) a[1][i] = Math.max(a[1][j] + a[0][i], a[1][i]);
+                if (a[1][i] > answer) answer = a[1][i];
             }
         }
-
-        // dp 배열에서 최대 값 찾기
-        int maxSum = dp[0];
-        for (int i = 1; i < N; i++) {
-            if (dp[i] > maxSum) {
-                maxSum = dp[i];
-            }
-        }
-
-        return maxSum;
+        System.out.println(answer);
     }
 }
