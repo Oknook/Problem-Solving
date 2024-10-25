4b17e28c9fb913aa04e9ee7f9cb5dff20bfb1487
Oknook <wjsguscjf23@naver.com>
2024-10-25 16:25:35 +0900

    [Silver III] Title: 삼각형 만들기, Time: 676 ms, Memory: 82492 KB -BaekjoonHub

--- 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
@@ -3,23 +3,22 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws Exception {
-        new Main().solution();
-    }
-
-    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        while (n-->0) arr[n] = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
         Arrays.sort(arr);
-        for (int i = arr.length-3; i >= 0; i--) {
-            if (arr[i] + arr[i+1] > arr[i+2]) {
-                System.out.println(arr[i] + arr[i+1] + arr[i+2]);
-                return;
+
+        int answer = -1;
+        for (int i = n-1; i >= 2; i--) {
+            if (arr[i-2] + arr[i-1] > arr[i]) {
+                answer = arr[i-2] + arr[i-1] + arr[i];
+                break;
             }
         }
-        System.out.println(-1);
+        System.out.println(answer);
     }
 }
