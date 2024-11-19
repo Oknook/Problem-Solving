d4f27391094a9b39de9010a6b32379e93e44b116
Oknook <wjsguscjf23@naver.com>
2024-11-19 18:06:17 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 148 ms, Memory: 22536 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -1,57 +1,37 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		Deque<int[]> q = new ArrayDeque<>();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int[] arr = new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("1 ");
-		int in = arr[0];
-		
-		for(int i=1; i<n; i++){
-			q.add(new int[] {(i+1), arr[i]}); // {풍선 idx, 내용}
-		}
-		
-		while(!q.isEmpty()) {
-			// 양수인 경우 
-			if(in >0) {
-				// 순서 뒤로 돌리기 
-				for(int i=1; i<in; i++) {
-					q.add(q.poll());
-				}
-				
-				int[] nxt = q.poll();
-				in = nxt[1];
-				sb.append(nxt[0]+" ");
-			}
-			// 음수인 경우 
-			else {
-				for(int i=1; i<-in; i++	) {
-					q.addFirst(q.pollLast());
-				}
-				
-				int[] nxt = q.pollLast();
-				in = nxt[1];
-				sb.append(nxt[0]+" ");
-			}
-		}
-				
-		System.out.println(sb.toString());
-		
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            deque.add(i);
+            map.put(i, Integer.parseInt(st.nextToken()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!deque.isEmpty()) {
+            int tmp = deque.pollFirst();
+            sb.append(tmp + " ");
+            if (deque.isEmpty()) break;
+
+            if (map.get(tmp) > 0) {
+                for (int i = 0; i < map.get(tmp)-1; i++) {
+                    deque.offerLast(deque.pollFirst());
+                }
+            }
+            else {
+                for (int i = 0; i > map.get(tmp); i--) {
+                    deque.offerFirst(deque.pollLast());
+                }
+            }
+        }
+        System.out.println(sb);
+    }
 }
