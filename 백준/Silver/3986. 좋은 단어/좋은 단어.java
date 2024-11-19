a0f68279fb44bdfefef3b7c4d3783028bcf8a7ad
Oknook <wjsguscjf23@naver.com>
2024-11-19 18:41:15 +0900

    [Silver IV] Title: 좋은 단어, Time: 388 ms, Memory: 22308 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -1,34 +1,41 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int answer = 0;
+    static Stack<Character> stack = new Stack<>();
+    static Stack<Character> tmp = new Stack<>();
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        int ans = 0;
-        while(n-- > 0) {
-            Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            boolean isGood = true;
-
-            int len = str.length();
-            for(int i = 0; i < len; i++) {
-                char ch = str.charAt(i);
-                if(stk.isEmpty()) {
-                    stk.push(ch);
-                }else {
-                    if(stk.peek() == ch) {
-                        stk.pop();
-                    }else {
-                        stk.push(ch);
-                    }
-                }
-            }
-            if(stk.isEmpty()) ans += 1;
+            if (str.length() % 2 == 0) check(str);
         }
-        System.out.println(ans);
+        System.out.println(answer);
+    }
+    static void check(String str) {
+        stack.clear();
+        tmp.clear();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+
+        while (!stack.isEmpty()) {
+            char c = stack.pop();
+
+            if (!tmp.isEmpty()) {
+                if (c == tmp.peek()) tmp.pop();
+                else if (!stack.isEmpty() && stack.peek() == c) stack.pop();
+                else tmp.push(c);
+            }
+            else {
+                if (c == stack.peek()) stack.pop();
+                else tmp.push(c);
+            }
+        }
+        if (tmp.isEmpty()) answer++;
     }
 }
