2e197ba4bb40963a5e32fb77dc694870f8ad1568
Oknook <wjsguscjf23@naver.com>
2025-09-29 23:27:59 +0900

    [level 2] Title: 전화번호 목록, Time: 851.43 ms, Memory: 200 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -13,5 +13,15 @@ class Solution {
             }
         }
         return true;
+        
+        // Arrays.sort(phoneBook);
+        // boolean result = true;
+        // for (int i=0; i<phoneBook.length-1; i++) {
+        //     if (phoneBook[i+1].contains(phoneBook[i])) {
+        //         result = false;
+        //         break;
+        //     }
+        // }
+        // return result;
     }
 }
