9cd72902f3127e10c0d629d98d622a5fb1e8f726
Oknook <wjsguscjf23@naver.com>
2025-09-29 23:25:17 +0900

    [level 2] Title: 전화번호 목록, Time: 859.74 ms, Memory: 200 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book, (o1, o2) -> o2.compareTo(o1));
+        HashSet<String> set = new HashSet<>();
+        for (String str : phone_book) {
+            if (set.contains(str)) return false;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.length(); i++) {
+                sb.append(str.charAt(i));
+                set.add(sb.toString());
+            }
+        }
+        return true;
+    }
+}
